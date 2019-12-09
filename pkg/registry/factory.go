d9a0bb17194e42bf7747140e5f230bcd0175b085
Grzegorz Polek <gpolek@g2a.com>
2019-12-09 11:56:49 +0100

    fix: fail when registries not passed

--- pkg/registry/factory.go
+++ pkg/registry/factory.go
@@ -16,13 +16,15 @@ func NewRegistriesMap(config *config.CmdRegistries) (map[string]*Registry, error
 		rm[DefaultRegistryPrefix] = defaultRegistry
 	}
 
-	for regName, registry := range *config {
-		if registry.RegistryType.Artifactory != nil {
-			reg, err := New(*registry.Artifactory)
-			if err != nil {
-				return rm, errors.Wrap(err, "failed to create registry object")
+	if config != nil {
+		for regName, registry := range *config {
+			if registry.RegistryType.Artifactory != nil {
+				reg, err := New(*registry.Artifactory)
+				if err != nil {
+					return rm, errors.Wrap(err, "failed to create registry object")
+				}
+				rm[regName] = reg
 			}
-			rm[regName] = reg
 		}
 	}
 
